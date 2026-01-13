d7c8e56d12bd0b2b9a91b536bb3ea1624d26ba29
Felipe <feliperebelatto87@gmail.com>
2026-01-13 19:19:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+gsap.registerPlugin(ScrollTrigger);
+
+var tl = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.two',
+        start: '0% 95%',
+        end: '70% 50%',
+        scrub: true,
+        markers: true,
+    },
+});
+
+tl.to(
+    "#fanta",
+    {
+        top: '120%',
+        left: '0%',
+    },
+    'orange'
+);
+
+tl.to(
+    '#laranja-cortada',
+    {
+        top: '160%',
+        left: '23%',
+    },
+    'orange'
+);
+
+tl.to(
+    '#laranja',
+    {
+        width: '15%',
+        top: '160%',
+        right: '10%',
+    },
+    'orange'
+);
+tl.to(
+    '#folha',
+    {
+        rotate: '530deg',
+        top: '110%',
+        left: '70%',
+    },
+    'orange'
+);
+tl.to(
+    '#folha2',
+    {
+        rotate: '530deg',
+        top: '110%',
+        left: '0%',
+    },
+    'orange'
+);
+
+
+var tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: '.three',
+        start: '0% 95%',
+        end: '20% 50%',
+        scrub: true,
+        markers: true,
+    },
+});
+
+tl2.from(
+    '.lemon',
+    {
+        rotate: '-90deg',
+        left: '-100%',
+        top: '100%',
+    },
+    'ca'
+
+);
+tl2.from(
+    '#cocacola, #pepsi',
+    {
+        rotate: '-90deg',
+        left: '-100%',
+        top: '100%',
+    },
+    'ca'
+
+);
+tl2.to(
+    '#laranja-cortada',
+    {
+        width: '18%',
+        left: '42%',
+        top: '204%',
+    },
+    'ca'
+
+);
+tl2.to(
+    '#fanta',
+    {
+        width: '35%',
+        left: '33%',
+        top: '210%',
+    },
+    'ca'
+
+);
